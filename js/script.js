6a4e2f392fca295ad2b0093bbfe0fae5266133e8
שלומי מלפוף <adiphoneseo@gmail.com>
2022-02-04 12:41:22 +0200

    this is my upload to github

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// cd
+// mkdir
+// touch index.html
+// touch script.js
+// touch style.css
+// touch README.md
+// touch package.json
+// npm init
+// npm install express --save
+// npm install body-parser --save
+// npm install ejs --save
+// npm install mongoose --save
+// npm install express-session --save
+// npm install connect-flash --save
+// npm install passport --save
+// npm install passport-local --save
+// npm install passport-local-mongoose --save
+// npm install express-validator --save
+// npm install express-session --save
+// npm install connect-flash --save
